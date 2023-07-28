8363758c6d9606fcc2f383ca856a39d2e113072c
Cam <camhack67@gmail.com>
2023-07-28 18:12:57 -0400

    js and layout setup tweeks

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
-const quizDiv = document.getElementById('quiz')
-const question = document.getElementById('question')
-const questionButton1 = document.getElementById('answer1')
-const questionButton2 = document.getElementById('answer2')
-const questionButton3 = document.getElementById('answer3')
-const questionButton4 = document.getElementById('answer4')
+// grabbing the location of 
+const quizDiv = document.querySelector('#quiz')
+const question = document.querySelector('#question')
+const questionButton1 = document.querySelector('#answer1')
+const questionButton2 = document.querySelector('#answer2')
+const questionButton3 = document.querySelector('#answer3')
+const questionButton4 = document.querySelector('#answer4')
+const timerH2 = document.querySelector('#timerH2')
 
 let highScores = JSON.parse(localStorage.getItem('highScores')) || []
-
-
-
+// starting time allowed for the test
 let timer = 90
+// current question being displayed
+let currentQuestion = [0]
 
+// timer countdown
 let timerId = setInterval(function(){
     timer -= 1
     console.log(timer)
 }, 1000)
 
-
-
-
-
 // questions
 let questions = [
-    {question: "lorem", answers: ["lorem1", "lorem2", "lorem3", "lorem4"], correctAnswer: 1}
-
-
+    {question: "lorem1", answers: ["lorem1", "lorem2", "lorem3", "lorem4"], correctAnswer: 1},
+    {question: "lorem2", answers: ["lorem1", "lorem2", "lorem3", "lorem4"], correctAnswer: 1},
+    {question: "lorem3", answers: ["lorem1", "lorem2", "lorem3", "lorem4"], correctAnswer: 1},
+    {question: "lorem4", answers: ["lorem1", "lorem2", "lorem3", "lorem4"], correctAnswer: 1},
+    {question: "lorem5", answers: ["lorem1", "lorem2", "lorem3", "lorem4"], correctAnswer: 1},
+    {question: "lorem6", answers: ["lorem1", "lorem2", "lorem3", "lorem4"], correctAnswer: 1}
 ]
-// current question being displayed
-let currentQuestion = 0
+
 
 // placeholder
 renderQuestion()
@@ -44,16 +45,18 @@ function renderQuestion(){
 }
 
 
-
+// event listener
 quizDiv.addEventListener('click', function(e){
-
+    // making sure the click only applies to the button
     if (e.target.matches('button')){
         console.log('clicked')
         console.log('value:' + e.target.innerText)
         console.log('correct answer:' + questions[currentQuestion].correctAnswer)
 
-        renderQuestion()
+        // moves to the next question
         currentQuestion++
+        // rerenders the question
+        renderQuestion()
 
     }
 
