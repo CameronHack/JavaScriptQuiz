e43f441cbfb74f884d9c3ac8eb7bbea5a246e847
Cam <camhack67@gmail.com>
2023-08-01 17:00:44 -0400

    added CLEAR DATA button to highscore screen

--- assets/JS/highscores.js
+++ assets/JS/highscores.js
@@ -7,3 +7,26 @@ for (var i = 0; i < highScores.length; i++) {
     // makes list items appear on screen
     highScoresList.appendChild(li);
 }
+
+
+
+// extra feature that I thought was necessary
+
+const clearData = document.querySelector('#clearData')
+
+// listener for the CLEAR DATA button
+clearData.addEventListener('click', function (e) {
+    if (e.target.matches('button')) {
+        // prompts user to confirm their choice to clear data
+        let clearScores = prompt('❗️ Warning this will clear all your previous submissions ❗️ Please type YES to continue');
+        if (clearScores === 'YES') {
+            // clears local storage
+            localStorage.clear()
+            window.alert("✅ CLEAR DATA SUCCESSFUL ✅")
+            // reloads the page
+            location.reload()
+        } else {
+            window.alert("❌ FAILED ❌ please type YES");
+        }
+    }
+})
