486509b75515c111ca22342cd37ae182bf63025c
Cam <camhack67@gmail.com>
2023-08-01 18:10:47 -0400

    cleaned up CSS and comments

--- assets/JS/highscores.js
+++ assets/JS/highscores.js
@@ -2,7 +2,7 @@
 for (var i = 0; i < highScores.length; i++) {
     const li = document.createElement("li");
     // creates a list item that displays users initials and score
-    li.textContent = highScores[i].initials + ' - ' + highScores[i].score;
+    li.textContent = highScores[i].initials + ' ~ ' + highScores[i].score;
     // makes list items appear on screen
     highScoresList.appendChild(li);
 }
